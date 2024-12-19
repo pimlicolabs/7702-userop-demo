ec5f610c6bb86e178403a3ee06cd9baf001ce6e0
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-12-19 22:10:18 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# 7702 Bundler Demo
+
+This demo showcases how to utilize EIP-7702 to sponsor a userOperation originating from a EOA by using the `pimlico_experimental_estimateUserOperationGas7702` and `pimlico_experimental_sendUserOperation7702` endpoints.
+
+This demo is ran on the Odyssey Testnet and uses a modified version of ETH-Infinitism's Simple Account to accommodate for EIP-7702. Instead of using a storage slot to hold the account signer/owner, the signer/owner is set to address(this).
+
+## Running
+
+```bash
+export PIMLICO_API_KEY=pim_xxxxxxxxxxxxxxxxxxxxxx
+
+pnpm install
+
+pnpm run start
+```
+
+### RPC Schemas
+
+pimlico_experimental_estimateUserOperationGas7702
+
+Inputs:
+```
+{
+  "jsonrpc": "2.0",
+  "method": "pimlico_experimental_estimateUserOperationGas7702",
+  "params": [
+    userOperation,
+    entryPoint,
+    signedAuthorization,
+    stateOverrides
+  ],
+  "id": 1
+}
+```
+
+pimlico_experimental_sendUserOperation7702
+
+Inputs:
+```
+{
+  "jsonrpc": "2.0",
+  "method": "pimlico_experimental_sendUserOperation7702",
+  "params": [
+    userOperation,
+    entryPoint,
+    signedAuthorization
+  ],
+  "id": 1
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,141 @@
+import {
+	createPublicClient,
+	createWalletClient,
+	Hex,
+	http,
+	zeroAddress,
+} from "viem";
+import {
+	generatePrivateKey,
+	privateKeyToAccount,
+	privateKeyToAddress,
+} from "viem/accounts";
+import { odysseyTestnet } from "viem/chains";
+import { eip7702Actions } from "viem/experimental";
+import { toSimpleSmartAccount } from "permissionless/accounts";
+import { createPimlicoClient } from "permissionless/clients/pimlico";
+import { createSmartAccountClient, deepHexlify } from "permissionless";
+import {
+	entryPoint07Address,
+	EstimateUserOperationGasReturnType,
+	UserOperation,
+} from "viem/account-abstraction";
+
+if (!process.env.PIMLICO_API_KEY) {
+	console.error("need env var PIMLICO_API_KEY");
+	process.exit(1);
+}
+
+const PRIVATE_KEY = generatePrivateKey();
+
+const pimlicoUrl = `https://api.pimlico.io/v2/${odysseyTestnet.id}/rpc?apikey=${process.env.PIMLICO_API_KEY}`;
+
+const walletClient = createWalletClient({
+	account: privateKeyToAccount(PRIVATE_KEY),
+	chain: odysseyTestnet,
+	transport: http(),
+}).extend(eip7702Actions());
+
+const publicClient = createPublicClient({
+	chain: odysseyTestnet,
+	transport: http(),
+});
+
+const pimlicoClient = createPimlicoClient({
+	transport: http(pimlicoUrl),
+});
+
+const main = async () => {
+	// Source code in contracts/src/SimpleAccount7702.sol
+	const SIMPLE_7702_ACCOUNT_IMPLEMENTATION =
+		"0x2A7Df271B4B48753EDd983ba163cB22374C7Bc89";
+
+	console.log(`sender: ${privateKeyToAddress(PRIVATE_KEY)}`);
+
+	const authorization = await walletClient.signAuthorization({
+		contractAddress: SIMPLE_7702_ACCOUNT_IMPLEMENTATION,
+		delegate: true, // We need to explicitly specify this to allow anyone to sponsor the type 4 transaction.
+	});
+
+	const smartAccount = await toSimpleSmartAccount({
+		address: walletClient.account.address,
+		client: publicClient,
+		owner: walletClient.account,
+	});
+
+	const smartAccountClient = createSmartAccountClient({
+		account: smartAccount,
+		bundlerTransport: http(pimlicoUrl),
+	});
+
+	// Fill out initial values.
+	let userOperation = {} as UserOperation;
+	userOperation.sender = walletClient.account.address;
+	userOperation.nonce = await smartAccount.getNonce();
+	userOperation.callData = await smartAccount.encodeCalls([
+		{ to: zeroAddress, value: 0n, data: "0x" },
+	]);
+	userOperation.signature = await smartAccount.getStubSignature(userOperation);
+
+	// Fill out gasPrices.
+	const gasInfo = (await pimlicoClient.getUserOperationGasPrice()).fast;
+	userOperation = {
+		...userOperation,
+		...gasInfo,
+	};
+
+	// Fill out stub paymaster data.
+	const stub = await pimlicoClient.getPaymasterData({
+		...userOperation,
+		chainId: odysseyTestnet.id,
+		entryPointAddress: entryPoint07Address,
+	});
+	userOperation = { ...userOperation, ...stub } as UserOperation;
+
+	// Estimate gas.
+	const gasEstimates = (await smartAccountClient.request({
+		// @ts-ignore
+		method: "pimlico_experimental_estimateUserOperationGas7702",
+		params: [
+			deepHexlify(userOperation),
+			// @ts-ignore
+			entryPoint07Address,
+			deepHexlify(authorization),
+		],
+	})) as EstimateUserOperationGasReturnType;
+	userOperation = { ...userOperation, ...gasEstimates } as UserOperation;
+
+	console.log({ gasEstimates });
+
+	// Get sponsor fields.
+	const sponsorFields = await pimlicoClient.getPaymasterData({
+		...userOperation,
+		chainId: odysseyTestnet.id,
+		entryPointAddress: entryPoint07Address,
+	});
+	userOperation = { ...userOperation, ...sponsorFields } as UserOperation;
+
+	// Sign and send user operation.
+	userOperation.signature = await smartAccount.signUserOperation(userOperation);
+	const userOperationHash = (await smartAccountClient.request({
+		// @ts-ignore
+		method: "pimlico_experimental_sendUserOperation7702",
+		// @ts-ignore
+		params: [
+			deepHexlify(userOperation),
+			// @ts-ignore
+			entryPoint07Address,
+			deepHexlify(authorization),
+		],
+	})) as Hex;
+
+	const { receipt } = await smartAccountClient.waitForUserOperationReceipt({
+		hash: userOperationHash,
+	});
+
+	console.log(
+		`UserOperation included: https://odyssey-explorer.ithaca.xyz/tx/${receipt.transactionHash}`,
+	);
+};
+
+main();
