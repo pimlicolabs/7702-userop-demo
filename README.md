4bb8e2a285d3dfbc38d14c9a34615acb84a062b5
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-01-23 16:00:55 +0000

    update docs

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This demo showcases how to utilize EIP-7702 to sponsor a userOperation originati
 
 This demo is ran on the Odyssey Testnet and uses a modified version of ETH-Infinitism's Simple Account to accommodate for EIP-7702. Instead of using a storage slot to hold the account signer/owner, the signer/owner is set to address(this).
 
+## Warning
+THIS IS A EXPERIMENTAL ENDPOINT AND SUBJECT TO CHANGE WITHOUT NOTICE.
+
 ## Running
 
 ```bash
@@ -37,18 +40,18 @@ pnpm run start
       "paymasterVerificationGasLimit",
       "paymasterPostOpGasLimit",
       "paymasterData",
-      "signature"
+      "signature",
+      "eip7702Auth": {
+          contractAddress: '0x2A7Df271B4B48753EDd983ba163cB22374C7Bc89',
+          chainId: '0xde9fb',
+          nonce: '0x',
+          r: '0xfb7daf8631a9a713b4d0d3255b2ee2ebfd7019435d156db7906a5d3ec33c566e',
+          s: '0x77874c6e19382a1a48b433f9dad5ac617dd18fd600de4053f979904694a5a138',
+          v: '0x1b',
+          yParity: '0x'
+      }
     },
     entryPoint,
-    { // Signed Authorization
-        contractAddress: '0x2A7Df271B4B48753EDd983ba163cB22374C7Bc89',
-        chainId: '0xde9fb',
-        nonce: '0x',
-        r: '0xfb7daf8631a9a713b4d0d3255b2ee2ebfd7019435d156db7906a5d3ec33c566e',
-        s: '0x77874c6e19382a1a48b433f9dad5ac617dd18fd600de4053f979904694a5a138',
-        v: '0x1b',
-        yParity: '0x'
-    },
     stateOverrides
   ],
   "id": 1
@@ -76,18 +79,18 @@ pnpm run start
       "paymasterVerificationGasLimit",
       "paymasterPostOpGasLimit",
       "paymasterData",
-      "signature"
+      "signature",
+      "eip7702Auth": {
+          contractAddress: '0x2A7Df271B4B48753EDd983ba163cB22374C7Bc89',
+          chainId: '0xde9fb',
+          nonce: '0x',
+          r: '0xfb7daf8631a9a713b4d0d3255b2ee2ebfd7019435d156db7906a5d3ec33c566e',
+          s: '0x77874c6e19382a1a48b433f9dad5ac617dd18fd600de4053f979904694a5a138',
+          v: '0x1b',
+          yParity: '0x'
+      }
     },
-    entryPoint,
-    { // Signed Authorization
-        contractAddress: '0x2A7Df271B4B48753EDd983ba163cB22374C7Bc89',
-        chainId: '0xde9fb',
-        nonce: '0x',
-        r: '0xfb7daf8631a9a713b4d0d3255b2ee2ebfd7019435d156db7906a5d3ec33c566e',
-        s: '0x77874c6e19382a1a48b433f9dad5ac617dd18fd600de4053f979904694a5a138',
-        v: '0x1b',
-        yParity: '0x'
-    },
+    entryPoint
   ],
   "id": 1
 }
--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,8 @@ if (!process.env.PIMLICO_API_KEY) {
 
 const PRIVATE_KEY = generatePrivateKey();
 
-const pimlicoUrl = `https://api.pimlico.io/v2/${odysseyTestnet.id}/rpc?apikey=${process.env.PIMLICO_API_KEY}`;
+//const pimlicoUrl = `https://api.pimlico.io/v2/${odysseyTestnet.id}/rpc?apikey=${process.env.PIMLICO_API_KEY}`;
+const pimlicoUrl = `http://0.0.0.0:8080/v2/${odysseyTestnet.id}/rpc?apikey=${process.env.PIMLICO_API_KEY}`;
 
 const walletClient = createWalletClient({
 	account: privateKeyToAccount(PRIVATE_KEY),
@@ -52,10 +53,11 @@ const main = async () => {
 
 	console.log(`sender: ${privateKeyToAddress(PRIVATE_KEY)}`);
 
-	const authorization = await walletClient.signAuthorization({
+	const signedAuthorization = await walletClient.signAuthorization({
 		contractAddress: SIMPLE_7702_ACCOUNT_IMPLEMENTATION,
 		delegate: true, // We need to explicitly specify this to allow anyone to sponsor the type 4 transaction.
 	});
+	console.log("Signed authorization:", signedAuthorization);
 
 	const smartAccount = await toSimpleSmartAccount({
 		address: walletClient.account.address,
@@ -79,6 +81,8 @@ const main = async () => {
 
 	// Fill out gasPrices.
 	const gasInfo = (await pimlicoClient.getUserOperationGasPrice()).fast;
+	console.log("Retrieved gas price info:", gasInfo);
+
 	userOperation = {
 		...userOperation,
 		...gasInfo,
@@ -90,6 +94,8 @@ const main = async () => {
 		chainId: odysseyTestnet.id,
 		entryPointAddress: entryPoint07Address,
 	});
+	console.log("Retrieved paymaster stub data:", stub);
+
 	userOperation = { ...userOperation, ...stub } as UserOperation;
 
 	// Estimate gas.
@@ -97,15 +103,14 @@ const main = async () => {
 		// @ts-ignore
 		method: "pimlico_experimental_estimateUserOperationGas7702",
 		params: [
-			deepHexlify(userOperation),
+			deepHexlify({ ...userOperation, eip7702Auth: signedAuthorization }),
 			// @ts-ignore
 			entryPoint07Address,
-			deepHexlify(authorization),
 		],
 	})) as EstimateUserOperationGasReturnType;
-	userOperation = { ...userOperation, ...gasEstimates } as UserOperation;
-
 	console.log({ gasEstimates });
+
+	userOperation = { ...userOperation, ...gasEstimates } as UserOperation;
 
 	// Get sponsor fields.
 	const sponsorFields = await pimlicoClient.getPaymasterData({
@@ -113,6 +118,8 @@ const main = async () => {
 		chainId: odysseyTestnet.id,
 		entryPointAddress: entryPoint07Address,
 	});
+	console.log("Retrieved sponsor fields:", sponsorFields);
+
 	userOperation = { ...userOperation, ...sponsorFields } as UserOperation;
 
 	// Sign and send user operation.
@@ -122,16 +129,17 @@ const main = async () => {
 		method: "pimlico_experimental_sendUserOperation7702",
 		// @ts-ignore
 		params: [
-			deepHexlify(userOperation),
+			deepHexlify({ ...userOperation, eip7702Auth: signedAuthorization }),
 			// @ts-ignore
 			entryPoint07Address,
-			deepHexlify(authorization),
 		],
 	})) as Hex;
+	console.log("User operation hash:", userOperationHash);
 
 	const { receipt } = await smartAccountClient.waitForUserOperationReceipt({
 		hash: userOperationHash,
 	});
+	console.log("User operation receipt:", receipt);
 
 	console.log(
 		`UserOperation included: https://odyssey-explorer.ithaca.xyz/tx/${receipt.transactionHash}`,
